04de810184cb65892db3af8ad85109f2791f22de
Badders80 <baddeley0@gmail.com>
2025-08-25 21:59:38 +1200

    SSR-safe FutureverseAuthProvider, env, and config updates

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+	experimental: {
+		serverActions: true,
+	},
+	transpilePackages: ['@futureverse/portal', '@futureverse/sso'],
 	webpack: (config, { isServer }) => {
 		if (!isServer) {
 			config.resolve.fallback = {
+				...config.resolve.fallback,
 				fs: false,
 				net: false,
 				tls: false,
